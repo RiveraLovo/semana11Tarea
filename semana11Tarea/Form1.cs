2eeb550ac75d2174a94ca833b8b9ac53d450a5fc
Steven Amílcar Quintanilla Villalta <USIS024020@ugb.edu.sv>
2021-10-18 13:25:05 -0600

    btn guardar

--- semana11Tarea/Form1.cs
+++ semana11Tarea/Form1.cs
@@ -29,8 +29,46 @@ namespace semana11Tarea
 
         private void bguardar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                MySqlConnectionmyConnection = newMySqlConnection(cadena_conexion);
+                stringmyInsertQuery = "INSERT INTO contactos(nombre,clave,nivel) Values(?nombre,?clave,?nivel)";
+                MySqlCommandmyCommand = newMySqlCommand(myInsertQuery);
 
+                myCommand.Parameters.Add("?nombre", MySqlDbType.VarChar, 40).Value = txtusuario.Text;
+                myCommand.Parameters.Add("?clave", MySqlDbType.VarChar, 45).Value = txtclave.Text;
+                myCommand.Parameters.Add("?nivel", MySqlDbType.Int32, 4).Value = lstnivel.Text;
+                myCommand.Connection = myConnection;
+                myConnection.Open();
+                myCommand.ExecuteNonQuery();
+                myCommand.Connection.Close();
+
+
+                MessageBox.Show("Usuario agregado con éxito", "Ok", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+                stringconsulta = "select * from contactos";
+
+                MySqlConnectionconexion = newMySqlConnection(cadena_conexion);
+                MySqlDataAdapterda = newMySqlDataAdapter(consulta, conexion);
+                System.Data.DataSetds = newSystem.Data.DataSet();
+                da.Fill(ds, "agenda");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "agenda";
+
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Ya existe el usuario", "Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            bnuevo.Visible = true;
+            bguardar.Visible = false;
+
+            txtusuario.Enabled = false;
+            txtclave.Enabled = false;
+            lstnivel.Enabled = false;
+            bnuevo.Focus();
+            }
         }
     }
-    
 }
